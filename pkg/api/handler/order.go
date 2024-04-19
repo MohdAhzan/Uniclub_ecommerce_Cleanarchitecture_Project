d0e10e340f00b1f3d4892449d0dbe8abce298404
MohdAhzan <gsikahzanpkm@gmail.com>
2024-04-19 18:20:31 +0530

    test Commit

--- pkg/api/handler/order.go
+++ pkg/api/handler/order.go
@@ -18,11 +18,8 @@ func NewOrderHandler(orderUseCase interfaces.OrderUseCase) *OrderHandler {
 	return &OrderHandler{
 
 		orderUseCase: orderUseCase,
-
 	}
 }
-
-
 
 func (u *OrderHandler) OrderFromCart(c *gin.Context) {
 
@@ -125,26 +122,25 @@ func (o *OrderHandler) CancelOrder(c *gin.Context) {
 	c.JSON(200, succesRes)
 }
 
+func (o *OrderHandler) ReturnOrder(c *gin.Context) {
 
-func (o *OrderHandler)ReturnOrder(c *gin.Context){
+	idString := c.Query("order_id")
 
-	idString:=c.Query("order_id")
-
-	orderID,err:=strconv.Atoi(idString)
-	if err!= nil {
-		errRes:=response.ClientResponse(http.StatusBadRequest,"error in string conversion",nil,err.Error())
-		c.JSON(http.StatusBadRequest,errRes)
-		return 
-	}
-
-	err=o.orderUseCase.ReturnOrder(orderID)
-	if err!= nil{
-		errRes:= response.ClientResponse(http.StatusBadRequest,"failed to return the order",nil,err.Error())
-		c.JSON(http.StatusBadRequest,errRes)
+	orderID, err := strconv.Atoi(idString)
+	if err != nil {
+		errRes := response.ClientResponse(http.StatusBadRequest, "error in string conversion", nil, err.Error())
+		c.JSON(http.StatusBadRequest, errRes)
 		return
 	}
 
-	successRes:=response.ClientResponse(http.StatusBadRequest,"This Order has been Returned",nil,err.Error())
-	c.JSON(http.StatusBadRequest,successRes)
+	err = o.orderUseCase.ReturnOrder(orderID)
+	if err != nil {
+		errRes := response.ClientResponse(http.StatusBadRequest, "failed to return the order", nil, err.Error())
+		c.JSON(http.StatusBadRequest, errRes)
+		return
+	}
+
+	successRes := response.ClientResponse(http.StatusBadRequest, "This Order has been Returned", nil, nil)
+	c.JSON(http.StatusBadRequest, successRes)
 
 }
